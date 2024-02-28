2e3479834a9c7c14d0c380b0da014771772fd80f
Sunsetboy <misha.sunsetboy@gmail.com>
2024-02-28 08:18:09 +0100

    tests update

--- src/k6/test_symfony.js
+++ src/k6/test_symfony.js
@@ -1,21 +1,26 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
 export const options = {
     vus: 10,
-    duration: '30s',
+    duration: "30s",
     maxRedirects: 0,
 };
 export default function () {
     // try to use not existing URL
-    http.get('http://app_symfony:8000/abcd1234');
+    http.get("http://app_symfony:8000/abcd1234");
     const requestBody = {
-        "url": "https://example.com"
-    }
+        url: "https://example.com",
+    };
     // create a URL and acccess it
-    const res = http.post('http://app_symfony:8000/api/url', JSON.stringify(requestBody))
+    const res = http.post(
+        "http://app_symfony:8000/api/url",
+        JSON.stringify(requestBody)
+    );
     if (res.status == 200) {
-        const shortUrl = JSON.parse(res.body).short_url
-        http.get('http://app_symfony:8000/' + shortUrl);
+        const shortUrl = JSON.parse(res.body).short_url;
+        for (let i = 0; i < 100; i++) {
+            http.get("http://app_symfony:8000/" + shortUrl);
+        }
     }
 
     sleep(1);
